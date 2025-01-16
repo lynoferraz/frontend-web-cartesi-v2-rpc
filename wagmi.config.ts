8be9ae9fb14bf066290da505eee81a9d5fcaf9d5
sandhilt <6170125+sandhilt@users.noreply.github.com>
2025-01-16 20:57:15 -0300

    chore: some changes

--- wagmi.config.ts
+++ wagmi.config.ts
@@ -1,11 +1,30 @@
 import { defineConfig } from "@wagmi/cli";
 import hardhatDeploy from "@sunodo/wagmi-plugin-hardhat-deploy";
+import { Application__factory, Outputs__factory, IERC1155__factory } from "@cartesi/rollups";
+
 // import { sepolia } from "@wagmi/core/chains"
+
+const BASE = "node_modules/@cartesi/rollups/export";
+
 export default defineConfig({
   out: "src/generated/rollups.ts",
+  contracts: [
+    {
+      abi: IERC1155__factory.abi,
+      name: "IERC1155",
+    },
+    {
+      abi: Outputs__factory.abi,
+      name: "Outputs",
+    },
+    {
+      abi: Application__factory.abi,
+      name: "ApplicationFactory",
+    },
+  ],
   plugins: [
     hardhatDeploy({
-      directory: "node_modules/@cartesi/rollups/export/abi",
+      directory: `${BASE}/abi`,
       includes: [
         /InputBox/,
         /CartesiDApp/,
